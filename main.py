570528a8d9f5afebdae87891018676de779c9ca6
Harpreet Singh <hsingh5@cisco.com>
2023-07-28 15:31:49 -0400

    demo-commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import importlib
+import sys
+from webex_bot.webex_bot import WebexBot
+from webex_bot.commands.echo import EchoCommand
+from labdevices import DevicesByHostname, RegisterDevice
+from subnets import SubnetByVlan
+from hyperflex import GetHyperflexAlarms
+from deployLab import DeployLab
+from runcommand import GetCommand
+
+api_token = "NzkzMWFkMDMtZGFlNS00NGQ3LThlYjAtZDVlZGI0Y2FkYmM1NDk1OTNmZDYtODhh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+
+bot = WebexBot(api_token, approved_domains=["cisco.com"], approved_users=["hsingh5@cisco.com"])
+
+bot.add_command(SubnetByVlan())    # Search any subnet by Vlan
+bot.add_command(DevicesByHostname()) # Search Device by hostname
+bot.add_command(RegisterDevice()) # Add Device 
+bot.add_command(GetHyperflexAlarms()) # Get Hyperflex Alarms
+bot.add_command(DeployLab())
+bot.add_command(EchoCommand())
+bot.add_command(GetCommand())
+bot.run()
