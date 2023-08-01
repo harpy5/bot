7315943e2c6c9e13a97fc075b000d9540ce4df5b
harpy5 <hsingh5@cisco.com>
2023-07-31 21:42:18 -0400

    creds

--- bot-webex/main.py
+++ bot-webex/main.py
@@ -3,9 +3,10 @@ import sys
 from webex_bot.webex_bot import WebexBot
 from webex_bot.commands.echo import EchoCommand
 from Initial_message import InitialRequest, AddNewLab
+import os
 
-api_token = "NzkzMWFkMDMtZGFlNS00NGQ3LThlYjAtZDVlZGI0Y2FkYmM1NDk1OTNmZDYtODhh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-
+api_token = os.environ.get('API_TOKEN')
+print(api_token)
 bot = WebexBot(api_token, approved_domains=["cisco.com"], approved_users=["hsingh5@cisco.com"])
 
 bot.add_command(EchoCommand())
